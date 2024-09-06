e4436f29e4763dca50c3477763c15c17401b25ef
PracticalEscapement <themoradis1@gmail.com>
2024-09-05 20:09:45 -0400

    House Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        House house1 = new House("Mike", 1);
+        House house1Copy = house1.deepCopy(house1);
+        System.out.println(house1);
+        System.out.println(house1Copy);
+
+        System.out.println(house1.getOwner());
+        System.out.println(house1Copy.getOwner());
     }
 }
