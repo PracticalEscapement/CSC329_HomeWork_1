771bb5bf79680afe1d46e154969f82255ec5f1cc
PracticalEscapement <themoradis1@gmail.com>
2024-09-18 14:12:40 -0400

    fix various bugs

--- src/main/java/org/example/PriorityQueueList.java
+++ src/main/java/org/example/PriorityQueueList.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import java.util.Objects;
+
+/**
+ * PriorityQueueList class implementing the PriorityQueue Interface
+ */
 public class PriorityQueueList implements PriorityQueue{
     public Node head;
     private int length;
 
+    //Default Constructor
     public PriorityQueueList() {
         this.head = null;
         this.length = 0;
     }
 
+    /**
+     * Returns Deep copy of a pql passed as a parameter
+     * @param pql PriorityQueueList
+     */
     // copy constructor
     public PriorityQueueList(PriorityQueueList pql) {
         this.length = 0;
@@ -18,14 +28,14 @@ public class PriorityQueueList implements PriorityQueue{
         while (current != null) {
             // Check if the current node is the head
             if (current == pql.head) {
-                this.head = new Node(current.data.deepCopy(current.data));
+                this.head = new Node(current.data.deepCopy());
                 location = this.head;
                 current = current.next;
                 this.length++;
                 continue;
             }
             // create new node for each occurrence of a node in the pql to copy
-            Node temp = new Node(current.data.deepCopy(current.data));
+            Node temp = new Node(current.data.deepCopy());
 
             location.next = temp;
             current = current.next;
@@ -35,19 +45,29 @@ public class PriorityQueueList implements PriorityQueue{
         }
     }
 
+    /**
+     * Adds new house to the pql
+     * @param house House
+     *
+     */
     @Override
-    public void addHouse(House a) {
-        Node temp = new Node(a);
+    public void addHouse(House house) {
+        Node temp = new Node(house);
         temp.next = this.head;
         this.head = temp;
         this.length++;
     }
 
+    /**
+     * Returns most expensive house and removes it from the list
+     * @return House
+     */
     @Override
     public House getMostExpensive() {
         Node max = this.head;
         Node current = this.head;
 
+        // First loop finds the object with the highest value
         while (current != null) {
             if (current.data.getValue() > max.data.getValue()) {
                 max = current;
@@ -56,6 +76,7 @@ public class PriorityQueueList implements PriorityQueue{
         }
         current = this.head;
         Node prev = current;
+        // Remove the highest value home and fixes pointers in the list
         while (current != null) {
             if (max == this.head) {
                 this.head = current.next;
@@ -88,7 +109,57 @@ public class PriorityQueueList implements PriorityQueue{
         return this.head == null;
     }
 
+    /**
+     * compares objects and returns boolean value based on equality
+     * @param o takes object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PriorityQueueList that = (PriorityQueueList) o;
+        if (this.length != that.length) {
+            return false;
+        }
 
+        Node thatCurrent = that.head;
+        Node thisCurrent = this.head;
+        while (thatCurrent != null) {
+            if (!Objects.equals(thisCurrent.data.getOwner(), thatCurrent.data.getOwner()) &&
+                    !Objects.equals(thisCurrent.data.getValue(), thatCurrent.data.getValue())) {
+                return false;
+            }
+            thisCurrent = thisCurrent.next;
+            thatCurrent = thatCurrent.next;
+        }
+        return true;
+    }
+
+    /**
+     * Take the current instance and returns a deep copy
+     * @return PriorityQueueList
+     */
+    public PriorityQueueList deepCopy() {
+        Node current = this.head;
+        PriorityQueueList pqlCopy = new PriorityQueueList();
+        Node copyCurrent = pqlCopy.head;
+        while (current != null) {
+            House house = current.data.deepCopy();
+            Node node = new Node(house);
+            if (current == this.head) {
+                pqlCopy.head = node;
+                copyCurrent = pqlCopy.head;
+            }
+            copyCurrent.next = current.next;
+            copyCurrent = copyCurrent.next;
+            current = current.next;
+        }
+        return pqlCopy;
+    }
+
+
+    // Temp method to do some testing
     public void show() {
         Node current = this.head;
         while (current != null) {
@@ -108,6 +179,7 @@ public class PriorityQueueList implements PriorityQueue{
             this.next = null;
         }
 
+        // For convenience, I added a constructor with parameters
         public Node(House data) {
             this.data = data;
             this.next = null;
